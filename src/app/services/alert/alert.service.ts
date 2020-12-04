ca93d16e6597494b9191f1a792c90aa0b3ec9c19
devcer <santosh.viswanatham@ask.com>
2020-12-05 01:09:42 +0530

    added alert service and configured with sweetalerts

---
+++ src/app/services/alert/alert.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import Swal from 'sweetalert2';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertService {
+
+  constructor() { }
+  showAlert(title = "", text = "", icon: 'success' | 'error'): Promise<any> {
+    return Swal.fire({
+      title, 
+      text,
+      icon
+    });
+  }
+}
